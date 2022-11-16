6a4ac4e59ffc8485385899a9e147b3ca38659e7d
200019097 <200019097@PTL510.ucsal.br>
2022-11-16 09:20:18 -0300

    Colocando postgres banco de dados

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -1,10 +1,31 @@
 package controller;
 
-public class Controller {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-	public static void main(String[] args) {
+@WebServlet(urlPatterns = {"/Controller", "/main"})
+
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 }
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -1,5 +1,9 @@
 package model;
 
 public class DAO {
-
+	/**Módulo de conexão**/
+	// Parâmetros de conexão
+	
+	private String driver = 
+	// Métodos de conexão
 }
